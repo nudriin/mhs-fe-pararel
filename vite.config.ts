593ad554839ffa907d3950324868edfea8664c32
Nurdin <mrsunnysummer@gmail.com>
2025-03-12 22:50:23 +0700

    chore: define proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://192.168.18.173:5000",
+                changeOrigin: true,
+            },
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
 })
